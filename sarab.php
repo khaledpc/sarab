7a2bf6479dee4509fc42a1e46d892fd3d7eae557
khaledpc <58152725+khaledpc@users.noreply.github.com>
2024-03-19 22:20:07 +0300

    Add files via upload

---
+++ sarab.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="ar">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta property="og:title" content="SARAB CHAT"/>
+        <meta property="og:description" content="سراب شات "/>
+        <meta property="og:image" content=""/>
+        <meta name="google" content="notranslate"/>
+        <meta name="csrf-token" content="NysF3q4aXkPPOtjeibBTDnzU2vJT51xfeMNtWEkJ">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, interactive-widget=resizes-content"/>
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+        <link rel="icon" type="image/x-icon" href="/uploads/c9kvfayi1r120npp.gif">
+        <title>SARAB CHAT</title>
+        <meta name="description" content="سراب">
+        <meta name="keywords" content="سراب شات"/>
+        <meta name="theme-color" content="#FFD7D7"/>
+        <meta name="msapplication-navbutton-color" content="#FFD7D7">
+        <meta name="apple-mobile-web-app-status-bar-style" content="#FFD7D7">
+        <link rel="stylesheet" href="/css/fa.css?v=2">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/login.css">
+        <link rel="stylesheet" href="/css/app.css?v=1709710885">
+        <style>
+ #middps {
+    transform: scale(1.7);
+}
+
+#middps {
+    margin-top: 32px !important;
+    margin-right: 20px !important;
+    margin-bottom: 30px !important;
+    margin-left: 46px !important;
+    display: inline-block !important;
+    visibility: visible !important;
+    opacity: 1 !important;
+}
+#connection_container #connection-state-off, #connection_container #connection-state-on {
+    position: relative;
+    font-size: .625rem;
+    background-color: #5cb85c;
+    color: #fff;
+    padding: .2em .4em;
+    border-radius: 0 .6em .6em 0;
+    left: 0;
+    z-index: 10;
+}
+            </style>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Aref+Ruqaa:wght@400;700&family=Noto+Nastaliq+Urdu:wght@400..700&display=swap" rel="stylesheet">
+            <script src="blabax.js?xyz=95d8865d1"></script>
+<link rel="manifest" href="pwa.webmanifest">
+<link rel="apple-touch-icon" href="app/256.png">
+<script>
+if('serviceWorker' in navigator){navigator.serviceWorker.register('pwa.js').then(/* console.log('Service Worker!') */)}
+</script>
+</head>
+        </head>
+        
+        <body>
+            <div id="app"><div>
+    <div id="login">
+        <div class='login_wrapper'>
+            <h1 id='login-header' class='border-color label label-primary'>
+    <a class='label label-primary login-header__label' href='/'>
+        <img src='https://alrai.com/uploads/images/2018/01/17/86762.jpg' width='28' height='30' />
+        <span class='login-header__title'>SARAB CHAT</span>
+        <button class='btn fr btn-success fa fa-refresh login-header__refresh'></button>
+    </a>
+</h1>
+                            <div id='login-banner' class='rainbow'>
+    <img  onerror='this.remove()' src='https://www.chabab-jo.com/uploads/jrx0fyagxho_o098.jpg' alt='banner' class='login-banner__img' />
+</div>                        <div id='login-btn-holder'>
+    <a href='https://www.facebook.com/' class='login-btn-holder__btn fa fa-facebook btn btn-primary'> فيسبوك </a>
+    <a href='/' class='login-btn-holder__btn fa fa-music btn btn-primary'> القوانين </a>
+    <a href='/' class='login-btn-holder__btn fa fa-star-half-o btn btn-primary'> الإشتراكات </a>
+    <a href='/' class='login-btn-holder__btn fa fa-user btn btn-primary'> الفعاليات </a>
+</div>
+<body class="x_global x_overal">
+
+<form action="index.php" method="post" onsubmit="if(form_submitted<1 &amp;&amp; this.username.value.trim().length>2){this.action='account.php';form_submitted=1;return true}else{return false}">
+
+<div id="middps" class="x_overal x_all_rounded sfade1" style="margin-top: 414px; display: block; visibility: visible; opacity: 1;">
+
+<div class="holder holder_menu">
+<div class="x1"></div>
+<div class="x2 x_accent_bb pointer" onclick="gourl('account.php?q=register')"></div><br style="clear:both"></div>
+
+<div class="holder"></div>
+
+<div class="holder">
+<input type="text" name="username" maxlength="16" class="holder_text x_accent_bb svg_user" placeholder="اسم المستخدم" value="">
+</div>
+
+<div class="holder" id="loginpass" style="">
+<input type="password" name="password" maxlength="32" class="holder_text x_accent_bb svg_pass" placeholder="كلمة المرور">
+</div>
+
+<div class="holder">
+<input type="submit" class="x_bcolor_z holder_button x_left_rounded" style="width:80%;" value="دخول"><input type="button" id="sguestbutton" class="x_accent_bg holder_button x_right_rounded svg_pass" style="width:20%" value="&nbsp;" onclick="switch_guest('اسم المسخدم','اسم المستخدم')" title="زائر | اسم المستخدم">
+</div>
+
+<div class="holder" style="text-align:right;font-size:80%;font-weight:bold;font-variant:small-caps">
+<div style="float:left" class="pointer" onclick="de('hidescreen_tran').style.display='block';hoop('flags_div',1)">
+<div class="svg_f_ar flag_curr"></div> </div>
+<div style="float:right" class="pointer x_accent_fg" onclick="gourl('account.php?q=password')"></div>
+</div>
+<br>&nbsp;<br>
+</div>
+
+<!-- Thank you for keeping this link or please purchase BlaB! AX Pro -->
+<div class="x_overal" style="font-size:80%;position:fixed;left:0;bottom:0;padding:3px;width:100%;text-align:center">
+Powered by <b><a class="x_accent_fg" href="https://kkkkkkkkkkkk.com" onclick="return hlink(this.href)">Mr.MSHKALGE</a></b></div>
+<!-- Thank you for keeping this link or please purchase BlaB! AX Pro -->
+
+
+<script>
+
+function switch_guest(g,u){
+if(allow_guest>0){
+a=document.forms[0].username
+b=de('loginpass')
+c=de('sguestbutton')
+if(guest_state<1){ guest_state=1; a.placeholder=g; hoop('loginpass',0); c.style.opacity='1.0'; document.forms[0].password.value='' }
+else{ guest_state=0;  a.placeholder=u; hoop('loginpass',1); c.style.opacity='0.5' }
+}}
+
+form_submitted=0; guest_state=1; show_passwordbox=false;
+allow_guest=parseInt(1);
+
+if(allow_guest<1){allow_guest=1;switch_guest('زائر','اسم المستخدم');allow_guest=0}
+else if(show_passwordbox){switch_guest('زائر','اسم المستخدم');}
+
+window.onload=verti_pos; window.onresize=verti_pos
+setTimeout("de('middps').style.display='none';de('middps').style.visibility='visible';hoop('middps',1)",100)
+
+function hlink(h){window.open(h);return false}
+
+</body>
+
+                                <div id='connection_container'>
+                                    <span class='connected' id='connection-state-on' style='display: none;'>متصل</span>
+                                    <span id='connection-state-off'>متصل</span>
+                                </div>
+                             
+                                <!-- error/success alerts -->
+                                <div class='alert alert-danger tabs-form-error error' style='display: none'></div>
+                                <div class='alert alert-success tabs-form-error success' style='display: none'></div>
+                                            <marquee class='marquee' direction='right' width='100%' scrolldelay='0' scrollamount='5'style='color: #FFFFFF; background-color: #0B1B6B'>
+    إدارة الفعاليات ؛ تتجدد فعالياتنا معكم اليوم بتمام الساعة العاشرة مساء مع الملهم علي ( مسابقة دينية رمضانية ) كونوا على الموعد مبدعينا الغالين بكم تزيد سعادتنا و تزيد بالبهجة أوقاتنا
+</marquee>            <div class='react-tabs'>
+
+                            </div>
+                            <div class='users-count-label'>
+                                <p class='label-count label label-primary'>
+                                    <span class='corner fa fa-user'>
+                                        <span class='count online'>0</span>
+                                    </span>
+                                    المتواجدون الآن 
+                                </p>
+                            </div>
+                        </div>
+                        
+                        <div id="online_users" onmousedown="m2down(event)" onmousemove="m2move(this,event)" class="online_users1 x_bcolor_x x_bottom_rounded" style="display: block;"
+                    </div>
+                    <div id="popup_container"></div>
+                </div>
+            </div>
+            <div></div>
+
+            </script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/ClientJS/0.1.11/client.min.js"></script>
+            <!-- <script src="khry.000webhostapp.com/js/bundle.js?"></script> -->
+            <script src="/js/app.js?v=1709710885"></script>
+            <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/6.4.0/adapter.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+            <script src="/js/ms.js"></script>
+            <script src="/js/ag.js"></script>
+            <script>
+                $(document).ready(function() {});
+            </script>
+            <script>
+
+function switch_guest(g,u){
+if(allow_guest>0){
+a=document.forms[0].username
+b=de('loginpass')
+c=de('sguestbutton')
+if(guest_state<1){ guest_state=1; a.placeholder=g; hoop('loginpass',0); c.style.opacity='1.0'; document.forms[0].password.value='' }
+else{ guest_state=0;  a.placeholder=u; hoop('loginpass',1); c.style.opacity='0.5' }
+}}
+
+form_submitted=0; guest_state=1; show_passwordbox=false;
+allow_guest=parseInt(1);
+
+if(allow_guest<1){allow_guest=1;switch_guest('زائر','اسم المستخدم');allow_guest=0}
+else if(show_passwordbox){switch_guest('زائر','اسم المستخدم');}
+
+window.onload=verti_pos; window.onresize=verti_pos
+setTimeout("de('middps').style.display='none';de('middps').style.visibility='visible';hoop('middps',1)",100)
+
+function hlink(h){window.open(h);return false}
+</script>
+        </body>
+    <body>
+</body>
+    </html>
